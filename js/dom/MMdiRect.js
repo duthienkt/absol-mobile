7a56f7d4628d310751ab7f30aab02ed60eb0ff86
duthienkt <blueskythien2010@live.com>
2020-05-04 17:53:07 +0700

    + MMdiRect

---
+++ js/dom/MMdiRect.js
@@ -0,0 +1,56 @@
+import '../../css/mmdirect.css';
+import Core from './Core';
+var _ = Core._;
+var $ = Core.$;
+
+
+function MMdiRect() {
+    this.$icon = $('.am-mdi-rect-icon', this);
+    this._iconName = undefined;
+}
+
+MMdiRect.render = function () {
+    return _({
+        class: 'am-mdi-rect',
+        child: 'span.mdi.am-mdi-rect-icon'
+    });
+};
+
+
+MMdiRect.property = {};
+
+/**
+ * @type {MMdiRect}
+ */
+MMdiRect.property.iconName = {
+    set: function (value) {
+        value = value || null;
+        if (this._iconName)
+            this.$icon.removeClass('mdi-' + this._iconName);
+        this._iconName = value;
+        if (this._iconName)
+            this.$icon.addClass('mdi-' + this._iconName);
+    },
+    get: function () {
+        return this._iconName;
+    }
+};
+
+
+MMdiRect.attribute = {};
+
+MMdiRect.attribute.iconName = {
+    set: function (value) {
+        this.iconName = value;
+    },
+    get: function () {
+        return this.iconName;
+    },
+    remove: function () {
+        this.iconName = undefined;
+    }
+};
+
+Core.install('mmdirect', MMdiRect);
+
+export default MMdiRect;
