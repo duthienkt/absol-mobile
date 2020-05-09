0f4b58d933cf58ef6ebc2388934d00416cabacfc
duthienkt <blueskythien2010@live.com>
2020-05-09 16:33:02 +0700

    + MConversation

---
+++ js/dom/MConversation.js
@@ -0,0 +1,139 @@
+import '../../css/mconversation.css';
+import Core from './Core';
+import QuickMenu from 'absol-acomp/js/QuickMenu';
+import OOP from 'absol/src/HTML5/OOP';
+var _ = Core._;
+var $ = Core.$;
+
+function MConversation() {
+    this._name = '';
+    this._shortContent = '';
+    this._time = '';
+    this._avatarSrc = null;
+    this.$name = $('.am-conversation-name', this);
+    this.$shortContent = $('.am-conversation-short-content', this);
+    this.$time = $('span.am-conversation-time', this);
+    this.$counter = $('.am-conversation-counter', this);
+    this.$avatar = $('.am-conversation-avatar', this);
+    OOP.drillProperty(this, this.$counter, 'counter', 'innerHTML')
+}
+
+MConversation.render = function () {
+    return _({
+        class: 'am-conversation',
+        child: [
+            {
+                class: 'am-conversation-avatar'
+            },
+            {
+                class: 'am-conversation-body',
+                child: [
+                    '.am-conversation-name',
+                    '.am-conversation-short-content',
+                ]
+
+            },
+            {
+                class: 'am-conversation-time-ctn',
+                child: 'span.am-conversation-time'
+            },
+            {
+                class: 'am-conversation-counter'
+            },
+            '.am-conversation-bottom-line'
+        ]
+    });
+};
+
+
+MConversation.property = {};
+
+/**
+ * @type {MConversation}
+ */
+MConversation.property.name = {
+    set: function (value) {
+        value = value || '';
+        this.$name.clearChild();
+        if (value) {
+            this.$name.addChild(_({ text: value }))
+        }
+        this._name = value;
+    },
+    get: function () {
+        return this._name;
+    }
+};
+
+
+/**
+ * @type {MConversation}
+ */
+MConversation.property.time = {
+    set: function (value) {
+        value = value || '';
+        this.$time.clearChild().addChild(_({ text: value }))
+        this._time = value;
+    },
+    get: function () {
+        return this._time;
+    }
+};
+
+/**
+ * @type {MConversation}
+ */
+MConversation.property.shortContent = {
+    set: function (value) {
+        if (typeof value == 'object') {
+            this.$shortContent.clearChild().addChild(_(value));
+        }
+        else {
+            this.$shortContent.innerHTML = value + '';
+        }
+        this._shortContent = value;
+    },
+    get: function () {
+        return this._shortContent;
+    }
+};
+
+/**
+ * @type {MConversation}
+ */
+MConversation.property.unread = {
+    set: function (value) {
+        if (value) {
+            this.addClass('am-status-unread');
+        }
+        else {
+            this.removeClass('am-status-unread');
+
+        }
+    },
+    get: function () {
+        return this.containsClass('am-status-unread')
+    }
+};
+
+
+MConversation.property.avatarScr = {
+    set: function (value) {
+        if (value) {
+            this.$avatar.addStyle('background-image', 'url(' + value + ')');
+        }
+        else {
+            value = null;
+            this.$avatar.removeStyle('background-image');
+        }
+        this._avatarSrc = value;
+    },
+    get: function () {
+        return this._avatarSrc;
+    }
+};
+
+Core.install('MConversation'.toLowerCase(), MConversation);
+
+
+export default MConversation;
