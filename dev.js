4c8a188c27d5ca1c4643852357a872333e5bbd8e
duthienkt <blueskythien2010@live.com>
2020-05-10 22:22:45 +0700

    - export module

--- dev.js
+++ dev.js
@@ -5,9 +5,35 @@ import MLeftNavigator from './js/dom/MLeftNavigator';
 import MNavigatorMenu from './js/dom/MNavigatorMenu';
 import MBottomTabbar from './js/dom/MBottomTabbar';
 import MTinyProfileBlock from './js/dom/MTinyProfileBlock';
+import MMatMenuButton from './js/dom/MMatMenuButton';
+import MMatMenuItem from './js/dom/MMatMenuItem';
+import MMatMenu from './js/dom/MMatMenu';
+import MHeaderBar from './js/dom/MHeaderBar';
+import BrowserDetector from 'absol/src/Detector/BrowserDetector';
+import Dom from 'absol/src/HTML5/Dom';
+import MConversation from './js/dom/MConversation';
+import MTabApplication from './js/mobile_app/MTabApplication';
+import MTabActivity from './js/mobile_app/MTabActivity';
+import MActivity from './js/mobile_app/MActivity';
 absol.coreDom.install({
     mnavigatormenu: MNavigatorMenu,
     mbottomtabbar: MBottomTabbar,
     mleftnavigator: MLeftNavigator,
-    mtinyprofileblock: MTinyProfileBlock 
+    mtinyprofileblock: MTinyProfileBlock,
+    mmatmenubutton: MMatMenuButton,
+    mmatmenuitem: MMatMenuItem,
+    mmatmenu: MMatMenu,
+    mheaderbar: MHeaderBar,
+    mconversation: MConversation
 });
+
+Object.assign(absol, {
+    MTabApplication: MTabApplication,
+    MTabActivity: MTabActivity,
+    MActivity: MActivity
+});
+
+if (BrowserDetector) Dom.documentReady.then(function () {
+    document.body.classList.add('am-mobile-theme');
+});
+
