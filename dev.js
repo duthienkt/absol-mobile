f89ad21355bb0489bb8373ffa8f66ed2ed8b73eb
duthienkt <blueskythien2010@live.com>
2020-06-18 21:50:02 +0700

    - export

--- dev.js
+++ dev.js
@@ -1,35 +1,15 @@
 //todo
 import 'absol/src/absol';
 import 'absol-acomp/dev';
-import MLeftNavigator from './js/dom/MLeftNavigator';
-import MNavigatorMenu from './js/dom/MNavigatorMenu';
-import MBottomTabbar from './js/dom/MBottomTabbar';
-import MTinyProfileBlock from './js/dom/MTinyProfileBlock';
-import MMatMenuButton from './js/dom/MMatMenuButton';
-import MMatMenuItem from './js/dom/MMatMenuItem';
-import MMatMenu from './js/dom/MMatMenu';
-import MHeaderBar from './js/dom/MHeaderBar';
 import BrowserDetector from 'absol/src/Detector/BrowserDetector';
 import Dom from 'absol/src/HTML5/Dom';
-import MConversation from './js/dom/MConversation';
 import MTabApplication from './js/mobile_app/MTabApplication';
 import MTabActivity from './js/mobile_app/MTabActivity';
 import MActivity from './js/mobile_app/MActivity';
-import MMessageInput from './js/dom/MMessageInput';
+import install from './js/dom/install';
 
-absol.coreDom.install({
-    mnavigatormenu: MNavigatorMenu,
-    mbottomtabbar: MBottomTabbar,
-    mleftnavigator: MLeftNavigator,
-    mtinyprofileblock: MTinyProfileBlock,
-    mmatmenubutton: MMatMenuButton,
-    mmatmenuitem: MMatMenuItem,
-    mmatmenu: MMatMenu,
-    mheaderbar: MHeaderBar,
-    mconversation: MConversation
-});
 
-absol.coreDom.install([MMessageInput]);
+install(absol.coreDom);
 
 Object.assign(absol, {
     MTabApplication: MTabApplication,
--- js/dom/install.js
+++ js/dom/install.js
@@ -1,19 +1,25 @@
-import MLeftNavigator from './js/dom/MLeftNavigator';
-import MNavigatorMenu from './js/dom/MNavigatorMenu';
-import MBottomTabbar from './js/dom/MBottomTabbar';
-import MTinyProfileBlock from './js/dom/MTinyProfileBlock';
-import MMatMenuButton from './js/dom/MMatMenuButton';
-import MMatMenuItem from './js/dom/MMatMenuItem';
-import MMatMenu from './js/dom/MMatMenu';
-import MHeaderBar from './js/dom/MHeaderBar';
-import MConversation from './js/dom/MConversation';
-import MMessageInput from './js/dom/MMessageInput';
+import MLeftNavigator from './MLeftNavigator';
+import MNavigatorMenu from './MNavigatorMenu';
+import MBottomTabbar from './MBottomTabbar';
+import MTinyProfileBlock from './MTinyProfileBlock';
+import MMatMenuButton from './MMatMenuButton';
+import MMatMenuItem from './MMatMenuItem';
+import MMatMenu from './MMatMenu';
+import MHeaderBar from './MHeaderBar';
+import MConversation from './MConversation';
+import MMessageInput from './MMessageInput';
+import MSelectListItem from './MSelectListItem';
+import MSelectList from './MSelectList';
+import MSelectMenu from './MSelectMenu';
 
 export default function install(core) {
     core.install([
         MMessageInput, MNavigatorMenu, MBottomTabbar,
         MLeftNavigator, MTinyProfileBlock, MMatMenuButton,
         MMatMenuItem, MMatMenu, MHeaderBar,
-        MConversation
+        MConversation,
+        MSelectListItem,
+        MSelectList,
+        MSelectMenu
     ]);
 }
