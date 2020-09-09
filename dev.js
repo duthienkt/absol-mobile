1e932db9c2685162688da8b3a89aff05d2813fe4
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-09-09 13:25:24 +0700

    - body height, to fix vh

--- dev.js
+++ dev.js
@@ -7,6 +7,8 @@ import MTabApplication from './js/mobile_app/MTabApplication';
 import MTabActivity from './js/mobile_app/MTabActivity';
 import MActivity from './js/mobile_app/MActivity';
 import install from './js/dom/install';
+import ResizeSystem from "absol/src/HTML5/ResizeSystem";
+import AElement from "absol/src/HTML5/AElement";
 
 
 install(absol.coreDom);
@@ -19,5 +21,16 @@ Object.assign(absol, {
 
 if (BrowserDetector.isMobile) Dom.documentReady.then(function () {
     document.body.classList.add('am-mobile-theme');
+    var bodySizeHookElt = _('attachhook').on('attached', function () {
+        ResizeSystem.add(this);
+        this.requestUpdateSize();
+    });
+    bodySizeHookElt.requestUpdateSize = function () {
+        var bodyBound = document.body.getBoundingClientRect();
+        document.body.style.setProperty('--body-width', bodyBound.width + 'px');
+        document.body.style.setProperty('--body-height', bodyBound.height + 'px');
+    };
+    document.body.appendChild(bodySizeHookElt)
 });
+
 
