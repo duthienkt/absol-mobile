708b111f961a1a59ac5eba08f86c6f590a823c87
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-09-25 14:16:53 +0700

    - MHSnapScroller

--- js/dom/MHSnapScroller.js
+++ js/dom/MHSnapScroller.js
@@ -1,6 +1,7 @@
 import '../../css/mhsnapscroller.css'
 import Core from "./Core";
 import AElement from "absol/src/HTML5/AElement";
+import ScrollSnap, {easeInOutQuad} from "./ScrollSnap";
 
 var _ = Core._;
 var $ = Core.$;
@@ -17,8 +18,12 @@ function MHSnapScroller() {
     this._scroll = {
         speed: 0,
         lastEventTime: 0,
-        homeGoing: false
+        homeGoing: false,
+        release: true,
+        homeX: 0,
+        x: 0
     };
+
 }
 
 MHSnapScroller.tag = 'MHSnapScroller'.toLowerCase();
@@ -30,28 +35,120 @@ MHSnapScroller.render = function () {
     });
 };
 
+MHSnapScroller.prototype._findHomeX = function () {
+    var bound = this.getBoundingClientRect();
+    var centerX = (bound.left + bound.right) / 2;
+    var eBounds = Array.prototype.map.call(this.childNodes, function (elt) {
+        return elt.getBoundingClientRect();
+    }).filter(function (bound) {
+        return bound.width > 0;
+    });
+    var scrollLeft = this.scrollLeft;
+    var nearestX = Infinity;
+    var nextX = Infinity;
+    var childCenterX;
+    for (var i = 0; i < eBounds.length; ++i) {
+        childCenterX = (eBounds[i].left + eBounds[i].right) / 2;
+        if (Math.abs(childCenterX - centerX) < Math.abs(nearestX - centerX)) {
+            nearestX = childCenterX;
+        }
+
+        if ((childCenterX - centerX >= 0) == (this._scroll.speed >= 0)) {
+            if (Math.abs(childCenterX - centerX) < Math.abs(nextX - centerX)) {
+                nextX = childCenterX;
+            }
+        }
+    }
+
+    if (((nearestX - centerX >= 0) != (this._scroll.speed >= 0)) && (nextX != Infinity)) {
+        nearestX = nextX;
+    }
+
+    return Math.max(0, Math.min(this.scrollWidth - this.clientWidth, scrollLeft + (nearestX - centerX)));
+
+};
+
 
 MHSnapScroller.prototype.startHomeGoing = function () {
-    if (this._scroll.homeGoing) return;
+    if (this._scroll.homeGoing || !this._scroll.release) return;
+    this._scroll.homeGoing = true;
+    if (this._scroll.speed < 50 && this._scroll.speed >= 0) {
+        this._scroll.speed = 50;
+    }
+    else if (this._scroll.speed > -50 && this._scroll.speed < 0) {
+        this._scroll.speed = -50;
+    }
+
+    this.eventHandler.homeGoingTick();
+
 };
 
 MHSnapScroller.prototype.stopHomeGoing = function () {
     if (!this._scroll.homeGoing) return;
+    this._scroll.homeGoing = false;
 };
 
 
+/***
+ *
+ * @type {{}|MHSnapScroller}
+ */
 MHSnapScroller.eventHandler = {};
 
-MHSnapScroller.eventHandler.touchstart = function (event) {
+MHSnapScroller.eventHandler.homeGoingTick = function (toFinish) {
+    if (!this._scroll.homeGoing) return;
+    if (toFinish===true){
+       this.stopHomeGoing();
+        return ;
+    }
+    var last = this._scroll.lastEventTime;
+    var now = new Date().getTime();
+    var dt = (now - last) / 1000;
+    var dx = dt * this._scroll.speed;
+    var limitDx = this._scroll.homeX - this._scroll.x;
+    var needContinue = false;
+    if (Math.abs(dx) >= Math.abs(limitDx) || dx * limitDx <= 0) {
+        this._scroll.x = this._scroll.homeX;
+    }
+    else {
+        this._scroll.x += dx;
+        needContinue = true;
+    }
 
+    this.scrollLeft = this._scroll.x;
+    if (needContinue)
+        requestAnimationFrame(this.eventHandler.homeGoingTick);
+    else
+        requestAnimationFrame(this.eventHandler.homeGoingTick.bind(this, true));
+}
+
+MHSnapScroller.eventHandler.stopScroll = function () {
+    var homeX = this._findHomeX();
+    this._scroll.homeX = homeX;
+    this.startHomeGoing();
 };
 
+/***
+ *
+ * @param {TouchEvent} event
+ */
+MHSnapScroller.eventHandler.touchstart = function (event) {
+    this.stopHomeGoing();
+    this._scroll.release = false;
+};
 
+/***
+ *
+ * @param {TouchEvent} event
+ */
 MHSnapScroller.eventHandler.touchend = function (event) {
-    console.log(event.touches.length);
+    if (event.touches.length === 0) {
+        this._scroll.release = true;
+    }
 };
 
 MHSnapScroller.eventHandler.scroll = function () {
+    if (this._scroll.homeGoing) return;
     var last = this._scroll.lastEventTime;
     var now = new Date().getTime();
     if (now - last > 100) {
@@ -64,6 +161,10 @@ MHSnapScroller.eventHandler.scroll = function () {
         this._scroll.lastEventTime = now;
         this._scroll.x = this.scrollLeft;
     }
+    if (this._scroll.stopTimeout > 0) {
+        clearTimeout(this._scroll.stopTimeout);
+    }
+    this._scroll.stopTimeout = setTimeout(this.eventHandler.stopScroll, 30);
 };
 
 export default MHSnapScroller;
