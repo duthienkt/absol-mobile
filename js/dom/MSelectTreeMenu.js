faa9145b972c49565e3ba72cbdc37c1d4e601a61
duthienkt <blueskythien2010@live.com>
2024-02-09 23:00:54 +0700

    - adapt old version

--- js/dom/MSelectTreeMenu.js
+++ js/dom/MSelectTreeMenu.js
@@ -1,49 +1,6 @@
-import '../../css/mselecttreemenu.css';
 import Core from "./Core";
-import MSelectMenu from "./MSelectMenu";
-import MTreeModal from "./MTreeModal";
+import MSelectTreeMenu from "absol-acomp/js/selecttreemenu/MSelectTreeMenu";
 
-var _ = Core._;
-var $ = Core.$;
-
-function MSelectTreeMenu() {
-    this._isFocus = false;
-    this._itemsByValue = {};
-    this.$holderItem = $('.am-selectmenu-holder-item', this);
-
-    /***
-     * @type {MTreeModal}
-     */
-    this.$selectlist = _('mtreemodal');
-
-    this.$selectlist.on('pressitem', this.eventHandler.pressItem, true)
-        .on('pressout', this.eventHandler.pressOut)
-        .on('pressclose', this.eventHandler.pressOut);
-    this.on('click', this.eventHandler.click, true);
-    this.$attachhook = $('attachhook', this).on('error', this.eventHandler.attached);
-}
-
-MSelectTreeMenu.tag = 'MSelectTreeMenu'.toLowerCase();
-
-MSelectTreeMenu.render = function () {
-    return  MSelectMenu.render().addClass('am-select-tree-menu');
-};
-
-Object.assign(MSelectTreeMenu.prototype, MSelectMenu.prototype);
-MSelectTreeMenu.property = Object.assign({}, MSelectMenu.property);
-MSelectTreeMenu.eventHandler = Object.assign({}, MSelectMenu.eventHandler);
-
-MSelectTreeMenu.prototype._dictByValue = function(items){
-    return items.reduce(function visitor(ac, cr) {
-        if (cr.items && cr.items.reduce){
-            cr.items.reduce(visitor, ac);
-        }
-        var value = cr.value+'';
-        ac[value] = cr;
-        return ac;
-    }, {})
-};
-
+export { default } from 'absol-acomp/js/selecttreemenu/MSelectTreeMenu';
+export * from 'absol-acomp/js/selecttreemenu/MSelectTreeMenu';
 Core.install(MSelectTreeMenu);
-
-export default MSelectTreeMenu;
--- js/dom/MTreeModal.js
+++ js/dom/MTreeModal.js
@@ -1,37 +1,4 @@
-import MListModal from "./MListModal";
-import Dom from "absol/src/HTML5/Dom";
-import treeListToList from "absol-acomp/js/list/treeListToList";
-import Core from "./Core";
-import {searchTreeListByText} from "absol-acomp/js/list/search";
+import { MTreeModal } from "absol-acomp/js/selecttreemenu/MSelectTreeMenu";
 
-/***
- * @extends MListModal
- * @constructor
- */
-function MTreeModal() {
-    MListModal.call(this);
-}
-
-
-MTreeModal.tag = 'MTreeModal'.toLowerCase();
-
-MTreeModal.render = function () {
-    return MListModal.render().addClass('am-tree-modal');
-};
-
-
-Object.assign(MTreeModal.prototype, MListModal.prototype);
-
-MTreeModal.prototype._listToDisplay = function (items) {
-    return treeListToList(items);
-};
-
-
-MTreeModal.property = Object.assign({}, MListModal.property);
-
-
-MTreeModal.eventHandler = Object.assign({}, MListModal.eventHandler);
-
-Core.install(MTreeModal);
 
 export default MTreeModal;
