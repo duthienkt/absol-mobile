1e4c3a26ee9ab946dce787bb421238219fcddd12
duthienkt <blueskythien2010@live.com>
2026-01-12 14:25:41 +0700

    - setup

---
+++ copy_dist.sh
@@ -0,0 +1,7 @@
+cp -R -f ./js/ ../absol-full/node_modules/absol-mobile/js/
+cp -R -f ./css/ ../absol-full/node_modules/absol-mobile/css/
+cp -f ./dev.js ../absol-full/node_modules/absol-mobile/
+
+cp -R -f ./js/ ../absol-form/node_modules/absol-mobile/js/
+cp -R -f ./css/ ../absol-form/node_modules/absol-mobile/css/
+cp -f ./dev.js ../absol-form/node_modules/absol-mobile/
